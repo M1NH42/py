cd5d5406709d8a841cca7545c0f5a9d24fb94c65
M1NH42 <minhazalam365@gmail.com>
2020-03-17 23:42:03 +0530

    Added function definition

---
+++ py basic/fxn.py
@@ -0,0 +1,17 @@
+# This program shows the working of the function
+# and ho wa a function is called
+
+# subtract
+def sub (a, b):
+    return a - b
+
+# addition
+def add (a, b):
+    return a + b
+
+# square
+def square (a):
+    return a * a
+
+print(square(3)) # square function called
+
