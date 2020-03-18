8cb9c7eedae10ff286aa27fe49ace116e2cc971f
M1NH42 <minhazalam365@gmail.com>
2020-03-18 23:47:35 +0530

    Bazinga!

--- py basic/state_exp.py
+++ py basic/state_exp.py
@@ -9,3 +9,5 @@ print(type(my_age))
 
 # add two variables
 print(my_age + len(my_name)) # length function is used to find length of a string
+
+print(((my_age // 13) ** 3) + ((len(my_name) ** 4) // 13))
