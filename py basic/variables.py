45945d4a575d0bdf01c9694700e7c4da0f57fd49
M1NH42 <minhazalam365@gmail.com>
2020-03-18 23:08:36 +0530

    Variables

---
+++ py basic/variables.py
@@ -0,0 +1,19 @@
+# This program shows the working of variables in python
+
+# variable declaration
+name = "Minhaz Alam"
+age = 22
+height = 5.7
+
+# print statements
+print(name)
+
+print(type(name))
+
+print(type(age))
+
+print(type(height))
+
+print(int(height))
+
+print(len(name))
