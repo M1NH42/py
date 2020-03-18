6c257b196ac2edb78c8b8f27154384730af4697b
M1NH42 <minhazalam365@gmail.com>
2020-03-18 23:42:55 +0530

    len() used

---
+++ py basic/state_exp.py
@@ -0,0 +1,11 @@
+# This program shows statements and expressions
+
+my_name = "Minhaz Alam"
+my_age = 22
+
+# prints data types of the variables
+print(type(my_name))
+print(type(my_age))
+
+# add two variables
+print(my_age + len(my_name)) # length function is used to 
