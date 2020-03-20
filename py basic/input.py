07ce57d9be4ed0159bab8756a0860eb0fbd4407b
M1NH42 <minhazalam365@gmail.com>
2020-03-20 20:38:15 +0530

    Take input from user

--- py basic/input.py
+++ py basic/input.py
@@ -1,2 +1,24 @@
 ## This program show how to take input from the user
 
+# addition function
+def add(x, y):
+    return x + y
+
+# subtraction function
+def sub(x, y):
+    return x - y
+
+# division function
+def div(x, y) :
+    return x / y
+
+# multiplication function
+def mul (x, y) :
+    return x * y
+
+x = input("Enter first number: ")
+first_number = int(x)
+
+y = input("Enter second number: ")
+second_number = int(y)
+
