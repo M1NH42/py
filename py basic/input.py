33b03e134c26f78a7ffbd5ee086905962d63213f
M1NH42 <minhazalam365@gmail.com>
2020-03-20 20:44:33 +0530

    Called different functions

--- py basic/input.py
+++ py basic/input.py
@@ -22,7 +22,17 @@ first_number = int(x)
 y = input("Enter second number: ")
 second_number = int(y)
 
+# calculate average of two numbers
 avg = add (first_number, second_number) / 2
 
+# prints average of two numbers
 print (avg)
 
+# prints addition of two numbers
+print(add (first_number, second_number))
+
+# prints difference between two given numbers
+print(sub(first_number, second_number))
+
+# prints multiplication of two numbers
+print(mul (first_number, second_number))
