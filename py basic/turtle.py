74adc2fe97e84701db6d549d3e022f5e0f0b5963
M1NH42 <minhazalam365@gmail.com>
2020-03-21 16:28:35 +0530

    Some changes!

--- py basic/turtle.py
+++ py basic/turtle.py
@@ -1,10 +1,21 @@
-import turtle             # allows us to use the turtles library
-wn = turtle.Screen()      # creates a graphics window
-alex = turtle.Turtle()    # create a turtle named alex
-alex.forward(150)         # tell alex to move forward by 150 units
-alex.left(90)             # turn by 90 degrees
-alex.forward(75)          # complete the second side of a rectangle
-alex.left(90)             # turn left by 90 degrees
-alex.forward(150)         # tell alex to move forward by 150 units
-alex.left(90)
-alex.forward(75)
+import turtle
+
+wn = turtle.Screen()
+bgc = input("Enter background for the canvas")
+wn.bgcolor(bgc)        # set the window background color
+
+tess = turtle.Turtle()
+tc = input("Enter turtle color")
+
+tess.color(tc)              # make tess blue
+
+p_size = input("Enter width of the turtle")
+p_size = int(p_size)
+tess.pensize(p_size)             # set the width of her pen
+
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.exitonclick()                # wait for a user click on the canvas
