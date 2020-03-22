b7b44fcad7229d899e1705e3d5ecbf5ec4bb3ab9
M1NH42 <minhazalam365@gmail.com>
2020-03-22 20:01:36 +0530

    Added full program with comments for clarity

--- py basic/range_fun.py
+++ py basic/range_fun.py
@@ -3,4 +3,23 @@
 # list
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# message
 print("Range(5): ")
+
+# iterate with range with single parameter
+for u in range(5):
+    print(u) # prints 0 to 4
+
+print("*******************")
+
+# iterate with range with two parameters
+for i in range (0, 5):
+    print(i) # prints 0 to 4
+
+print("*******************")
+
+# casting range to list using list function
+print(list(range(5)))
+
+# casting with range using two variables using list function
+print(list(range(0, 5)))
