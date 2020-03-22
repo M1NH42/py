0373257a16e8294d3c16d48d4b94d6f5a462ca83
M1NH42 <minhazalam365@gmail.com>
2020-03-22 19:05:48 +0530

    For loop

---
+++ py basic/iteration.py
@@ -0,0 +1,12 @@
+# in this program we'll implement iteration in python
+# language as shown in this program
+
+# asks user to enter his/her name
+name = input ("Enter your name ")
+
+# break
+print ("****************************")
+
+# iterate through the number of characters in name variable 
+for char in name :
+    print(char)
