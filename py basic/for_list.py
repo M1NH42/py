11e20f488b375b8fda7a41c1f73dea50ccbd68e4
M1NH42 <minhazalam365@gmail.com>
2020-03-22 19:13:16 +0530

    for loop with lists

---
+++ py basic/for_list.py
@@ -0,0 +1,8 @@
+# we'll implement for loop with lists in this program
+
+# list define
+city = ["paris", "netherland", "london", "singapore"]
+
+# iterating through the list
+for i in city :
+    print (i)
