855458eebf9a1af57ecef5530e2eb8568f2dafa0
M1NH42 <minhazalam365@gmail.com>
2020-03-22 17:47:37 +0530

    list and string operation using some functions

---
+++ py basic/list_strings.py
@@ -0,0 +1,11 @@
+# In this program we'll perform various string and list operations
+# such as split(), count(), index(), join()
+
+# string
+song = "go corona, come kareena"
+
+# splits through "o"
+s = song.split("o")
+
+# prints s (after splitting through 'o')
+print(s)
