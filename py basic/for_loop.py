f715f8b4f7da08bccd5f9cdfc7a4b6a23a13b263
M1NH42 <minhazalam365@gmail.com>
2020-03-23 14:01:36 +0530

    Traversal using for loop with comments for clarity

---
+++ py basic/for_loop.py
@@ -0,0 +1,9 @@
+# this program is the implementation of the traversal using
+# for loop as shown below
+
+# list containing fruits items
+fruits = ["banana", "apple", "grapes", "chikku", "santra"]
+
+# traversal through the list using for loop
+for i in range(5) :
+    print(i, fruits[i])
