4784fb9c26317700334823cbeb39c055eee44978
M1NH42 <minhazalam365@gmail.com>
2020-03-23 15:50:31 +0530

    Comments for clarity

--- py basic/intermediate_it.py
+++ py basic/intermediate_it.py
@@ -2,14 +2,21 @@
 # using print statement we can easily know the values
 # of the variables
 
+# store numbers 0 to 9 in 'w'
 w = range(10)
 
+# assign 0 to tot
 tot = 0
+
+# print a message
 print("***** Before the For Loop ******")
+
 for num in w:
     print("***** A New Loop Iteration ******")
     print("Value of num:", num)
     tot += num
     print("Value of tot:", tot)
+
 print("***** End of For Loop *****")
+
 print("Final total:", tot)
