9cb0dbabe4570ceaf68701054a87041e1b8bb789
M1NH42 <minhazalam365@gmail.com>
2020-03-24 13:58:45 +0530

    Checked on numbers (integers) also

--- py basic/boolean_exp.py
+++ py basic/boolean_exp.py
@@ -19,8 +19,11 @@ print(not x)
 print("********************************")
 ## we'll perfrom "in" in list to evaluate boolean expression
 
-# list
+# list of strings
 names = ["adam", "eve", "peter", "henry", "william"]
+
+# list of integer values
+numbers = [1, 2, 3, 4, 5, 6, 7, 0, 8]
 
 # ask user to enter a string
 user_input = input("Enter a string to check: ")
@@ -28,6 +31,13 @@ user_input = input("Enter a string to check: ")
 # print boolean value after evaluation
 print(user_input in names)
 
+print("********************************")
+# evaluate numbers
+number = input("Enter a number")
+
+number = int(number)
+
+print(number in numbers)
 
 print("********************************")
 ## we'll perfrom for string 
