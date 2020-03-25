8249fa0559ef148c06812e98a0b05f5546a1478b
M1NH42 <minhazalam365@gmail.com>
2020-03-25 20:23:39 +0530

    Added conditional statements

--- py basic/conditionals.py
+++ py basic/conditionals.py
@@ -1,4 +1,17 @@
 # in this program we will implement conditional operations
 # available in python 3
 
-first_num = int(input(""))
+first_num = input("Enter a number: ")
+first_num = int(first_num)
+
+second_num = input("Enter another number: ")
+second_num = int(second_num)
+
+if first_num > second_num :
+    print(first_num, "is greater")
+    
+elif first_num < second_num :
+    print(first_num, "is smaller")
+
+else :
+    print("Both the numbers are equal")
