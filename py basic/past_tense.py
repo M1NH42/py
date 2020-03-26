703ddf19a412da94fdd0112ad8926ae56570396c
M1NH42 <minhazalam365@gmail.com>
2020-03-26 18:05:39 +0530

    The logic with for and if

--- py basic/past_tense.py
+++ py basic/past_tense.py
@@ -3,3 +3,19 @@
 # Otherwise, add ‘ed’ to make it past tense. 
 # Save these past tense words to a list called past_tense
 
+# *************************** PROGRAM **************************
+
+# list of words
+words = ["adopt", "bake", "beam", "confide", "grill", "plant", "time", "wave", "wish"]
+
+# init new list
+past_tense = []
+
+# iterate
+for word in words :
+    if word.endswith('e') :
+        past_tense.append(word + 'd')
+    else :
+        past_tense.append(word + 'ed')
+
+# print the new list of past tense
