07c9929a7ea1c3ed6eb8b5f76c890b90834da9bd
M1NH42 <minhazalam365@gmail.com>
2020-03-27 12:55:06 +0530

    Comments for clarity

--- py basic/str_muta.py
+++ py basic/str_muta.py
@@ -1,7 +1,14 @@
 ### Since strings are not mutable we can still modify the 
 ### string by using below method
 
+# string
 greeting = "Hello, world!"
+
+# new string using previous one
 newGreeting = 'J' + greeting[1:]
+
+# print new string
 print(newGreeting)
+
+# print original string
 print(greeting)          # same as it was
