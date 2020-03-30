d7aa6d9ae375f7450d38c5466dc02af0702c7624
M1NH42 <minhazalam365@gmail.com>
2020-03-30 19:32:23 +0530

    About program

--- py dict fun and files/programs/read_file.py
+++ py dict fun and files/programs/read_file.py
@@ -1,3 +1,7 @@
+# ABOUT : The program deals with the working of files system
+#       : Different methods to deal with files
+#       * open("file_name", "mode of operation")
+
 file_ref = open("olympic.txt", "r")
 
 # print(file_ref)
