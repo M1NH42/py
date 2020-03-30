80f334bdbb031caa83ed3ba571de81018e21b6de
M1NH42 <minhazalam365@gmail.com>
2020-03-30 19:33:10 +0530

    Comments for clarity

--- py dict fun and files/programs/read_file.py
+++ py dict fun and files/programs/read_file.py
@@ -2,8 +2,10 @@
 #       : Different methods to deal with files
 #       * open("file_name", "mode of operation")
 
+# open file and link the file object to variable
 file_ref = open("olympic.txt", "r")
 
 # print(file_ref)
 
+# closes the file object
 file_ref.close()
