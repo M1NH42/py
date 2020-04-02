a2b8a7582276cbc2071df23ad6bcd7dc5db81f30
M1NH42 <minhazalam365@gmail.com>
2020-04-02 14:38:12 +0530

    Added logic to write to .csv file

---
+++ py dict fun and files/programs/csv_writing.py
@@ -0,0 +1,20 @@
+# ABOUT : This program deals with the writing of the .csv files using
+#         python 3 code as shown
+
+olympians = [("John Aalberg", 31, "Cross Country Skiing"),
+             ("Minna Maarit Aalto", 30, "Sailing"),
+             ("Win Valdemar Aaltonen", 54, "Art Competitions"),
+             ("Wakako Abe", 18, "Cycling")]
+
+out_file = open("reduced_olympics.csv", "w")
+
+out_file.write('Name, Age, Sport')
+
+out_file.write('\n')
+
+for olympian in olympians :
+    row_string = '{}, {}, {}'.format(olympians[0], olympians[1], olympians[2])
+    out_file.write(row_string)
+    out_file.write('\n')
+print(out_file)
+out_file.close()
