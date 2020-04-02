bbbf25a3879c28f63198dd9bc553d061a2355fa1
M1NH42 <minhazalam365@gmail.com>
2020-04-02 16:33:07 +0530

    Dictionary declaration in python 3

--- py dict fun and files/programs/dictionary.py
+++ py dict fun and files/programs/dictionary.py
@@ -1,3 +1,15 @@
 # ABOUT : This programdeals with the dictionary
 #         which special python built-in mapping
 #         type that contain a (key, value) pair
+#         * unordered
+#         * associative collection
+
+eng2sp = {}
+
+eng2sp['one'] = 'uno'
+
+eng2sp['two'] = 'dos'
+
+eng2sp['three'] = 'tres'
+
+print(eng2sp)
