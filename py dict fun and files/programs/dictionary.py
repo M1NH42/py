c5b773c10dda9f81199f4c40c239932857e09fd1
M1NH42 <minhazalam365@gmail.com>
2020-04-02 16:33:16 +0530

    Comments for clarity

--- py dict fun and files/programs/dictionary.py
+++ py dict fun and files/programs/dictionary.py
@@ -4,12 +4,17 @@
 #         * unordered
 #         * associative collection
 
+# declare an empty dictionary
 eng2sp = {}
 
+# store 'uno' to key 'one'
 eng2sp['one'] = 'uno'
 
+# link '' to key ''
 eng2sp['two'] = 'dos'
 
+# link value 'tres' to key 'three'
 eng2sp['three'] = 'tres'
 
+# print the dictionary
 print(eng2sp)
