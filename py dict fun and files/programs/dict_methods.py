638e6c92e8e5e9130a06648446906c9e923841ad
M1NH42 <minhazalam365@gmail.com>
2020-04-03 20:07:42 +0530

    Iterating through a dictionary using for loop

--- py dict fun and files/programs/dict_methods.py
+++ py dict fun and files/programs/dict_methods.py
@@ -1,2 +1,7 @@
 # ABOUT : In this program we will implement some of the important methods
 #         we can use with dictionary data structure in python 3
+
+inventory = {'apples': 122, 'oranges': 321, 'bananas': 30, 'grapes': 455}
+
+for key in inventory:
+    print(key, 'got value: ', inventory[key])
