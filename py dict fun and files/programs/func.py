6290138b933f2b62703466a2c85d2daaf33de977
M1NH42 <minhazalam365@gmail.com>
2020-04-05 15:43:51 +0530

    Comments for clarity

--- py dict fun and files/programs/func.py
+++ py dict fun and files/programs/func.py
@@ -1,13 +1,22 @@
 # *About* : We'll define various function to implement our program
 
+# define sum(param)
 def sum(lst):
+    # accum variable
     tot = 0
+    # iterate through every num and 
+    # keep on adding to num tot
     for num in lst :
         tot += num
+    # returns total
     return tot
 
+# define product(by passing list as param)
 def prod(lst):
+    # initialize prod
     res = 1
+    # iterate through every num in the list
+    # and keep on mult to num res
     for num in lst :
         res *= num
     return res
