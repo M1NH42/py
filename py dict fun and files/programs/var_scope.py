689ccabd0a6171414fd8cf8f253c5634d393ac8a
M1NH42 <minhazalam365@gmail.com>
2020-04-06 12:09:23 +0530

    Comments for clarity

--- py dict fun and files/programs/var_scope.py
+++ py dict fun and files/programs/var_scope.py
@@ -2,19 +2,24 @@
 # * global variable
 # * local variable
 
+# Define square funcions
 def square(num) :
     sq = num * num
     return sq
 
+# Add sum of square fxn
 def sum_of_square(fn, sn, tn) :
-    x = square(fn)
-    y = square(sn)
-    z = square(tn)
+    x = square(fn) # called square function
+    y = square(sn) # called square function
+    z = square(tn) # called square function
 
+    # calculates sum of squares
     sos = x + y + z
 
+    # returns the sum
     return sos
 
+# prints sum of squares
 print(sum_of_square(2, 3, 4))
 
-print(sos)
+print(sos) # results in error because sos is a local variable to sum_of_square
