a3793d5bc2a0eb704663be4fc241e011aefb0268
M1NH42 <minhazalam365@gmail.com>
2020-04-06 12:45:37 +0530

    Add tuple packing

---
+++ py dict fun and files/programs/tuple_packing.py
@@ -0,0 +1,6 @@
+# About : In this program we will implement tuple packing
+# can be shown below with detailed description as well
+
+bond = ("James", "Bond", 1999, "Actresses", "Guns", "Cars")
+
+print(bond [4])
