33fe5fb2cf1db9cf14606c1ec3de7420b6a5c489
M1NH42 <minhazalam365@gmail.com>
2020-04-06 12:52:37 +0530

    Add logic

--- py dict fun and files/programs/tup_pack.py
+++ py dict fun and files/programs/tup_pack.py
@@ -1,4 +1,11 @@
 # About : This is the implementation of the tuple packing functionality
 # and its application in function return statement
 
-def circle
+def circle_info(radius) :
+    circum = 2 * 3.14 * radius
+
+    area_cir = 3.14 * radius * radius
+
+    return circum, area_cir
+
+print(circle_info(10))
