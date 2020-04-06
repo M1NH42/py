0ee473157eb8d90cc9030f1b6eed5555e98f86d4
M1NH42 <minhazalam365@gmail.com>
2020-04-06 12:54:06 +0530

    Comments for clarity

--- py dict fun and files/programs/tup_pack.py
+++ py dict fun and files/programs/tup_pack.py
@@ -1,11 +1,15 @@
 # About : This is the implementation of the tuple packing functionality
 # and its application in function return statement
 
+
+# define func and pass radius as param
 def circle_info(radius) :
+    # calculate circumference
     circum = 2 * 3.14 * radius
-
+    # calculate area of the circle
     area_cir = 3.14 * radius * radius
-
+    # return both as tuple
     return circum, area_cir
 
+# prints in the form of tuple
 print(circle_info(10))
