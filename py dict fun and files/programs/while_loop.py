a70f7ab333d71fb68024670e194953710a9b4d38
M1NH42 <minhazalam365@gmail.com>
2020-04-07 23:00:00 +0530

    Some changes

--- py dict fun and files/programs/while_loop.py
+++ py dict fun and files/programs/while_loop.py
@@ -3,18 +3,24 @@
 
 # function define
 # num as param
+
 def to_sum(num) :
+
     # init a_num
     a_num = 1
+
     # init sum so far
     sum_so_far = 0
 
     # condition while true do this...
     while a_num <= num :
+
         # Add num to sum
         sum_so_far += a_num
+
         # increment a number by one (1)
         a_num += 1
+        
     # return the sum of numbers
     return sum_so_far
 
