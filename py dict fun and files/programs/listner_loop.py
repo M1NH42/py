965be14e61a555470e549120cbf801dbf1f7f0b5
M1NH42 <minhazalam365@gmail.com>
2020-04-08 20:53:12 +0530

    Add logic

--- py dict fun and files/programs/listner_loop.py
+++ py dict fun and files/programs/listner_loop.py
@@ -1,3 +1,9 @@
 # About : In this we'll implement the listner loop
 # also known as while loop as shown
 
+theSum = 0
+x = -1
+while (x != 0):
+    x = int(input("next number to add up (enter 0 if no more numbers): "))
+    theSum = theSum + x
+print(theSum)
