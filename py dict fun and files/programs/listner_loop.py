58110727cba8c49865e01a92b576456d2cd2e2d1
M1NH42 <minhazalam365@gmail.com>
2020-04-08 20:55:15 +0530

    Some changes

--- py dict fun and files/programs/listner_loop.py
+++ py dict fun and files/programs/listner_loop.py
@@ -10,9 +10,12 @@ x = -1
 # while loop to iterate
 # checks if x equals 0 or not
 while (x != 0):
+
     # Message to enter a number
     x = int(input("next number to add up (enter 0 if no more numbers): "))
+
     # update sum
     theSum = theSum + x
+    
 # prints sum
 print(theSum)
