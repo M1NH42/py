68e24b2284736ab4eff2650e4db72215cd4315cb
M1NH42 <minhazalam365@gmail.com>
2020-04-09 11:58:28 +0530

    Add program description

--- py dict fun and files/programs/grocery.py
+++ py dict fun and files/programs/grocery.py
@@ -1,1 +1,4 @@
-# About :
+# About : This is the implementation of while loop to calculate
+# total price of the grocery by adding prices of each item
+# and then returning the total and average amount of the grocery
+
