b01258e0d9ab7ef3bc705da2fa63e2ae9a9eb1c9
M1NH42 <minhazalam365@gmail.com>
2020-04-09 14:21:51 +0530

    Add logic

--- py dict fun and files/programs/grocery.py
+++ py dict fun and files/programs/grocery.py
@@ -2,3 +2,21 @@
 # total price of the grocery by adding prices of each item
 # and then returning the total and average amount of the grocery
 
+def checkout():
+    total = 0
+    count = 0
+    moreItems = True
+    while moreItems:
+        price = float(input('Enter price of item (0 when done): '))
+        if price != 0:
+            count = count + 1
+            total = total + price
+            print('Subtotal: $', total)
+        else:
+            moreItems = False
+    average = total / count
+    print('Total items:', count)
+    print('Total $', total)
+    print('Average price per item: $', average)
+
+checkout()
