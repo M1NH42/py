03768d7b3921bf302dcf4492d643e46e71a6e455
M1NH42 <minhazalam365@gmail.com>
2020-04-10 11:27:55 +0530

    Add description and few lines of code

--- py dict fun and files/programs/sort_f.py
+++ py dict fun and files/programs/sort_f.py
@@ -1,2 +1,12 @@
 # About : In this program we will implement/use built-in
 # sorting function in python language
+
+# first we'll use the method sort()
+# which does not return anything but actually modifies
+# the original list
+
+# a list of numbers
+l1 = [-2, 2, 1, 0, -9, 73, -32]
+
+# called sort() method
+l1.sort()
