72003b2e9090800fdf0ae29760b9ee872674eb6e
M1NH42 <minhazalam365@gmail.com>
2020-04-12 20:44:52 +0530

    Add logic

--- data collectn and processing/week 1/nested_list.py
+++ data collectn and processing/week 1/nested_list.py
@@ -3,3 +3,22 @@
 # In this course we'll learn and implement about data collecyion and
 # processing of that data
 
+# this prgram is baout nested lists and how indexing can be used to 
+# extract information from the nested list
+
+# nested list
+animals = [['cat', 'dog', 'mouse'], ['horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+
+# iteration
+for animal in animals :
+    print(animal)
+
+# index help
+print(animals[1][2])
+
+# incremental approach
+# store internal 2nd list in new list
+new_list = animals [2]
+
+# print index 2of new list
+print(new_list [2])
