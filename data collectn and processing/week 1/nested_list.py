4efaa077035e383e5476509bb5176ef8080a1437
M1NH42 <minhazalam365@gmail.com>
2020-04-12 20:17:13 +0530

    Add program description

---
+++ data collectn and processing/week 1/nested_list.py
@@ -0,0 +1,5 @@
+# We are heading towards the week 1 of third course of the python 3
+# specialization...
+# In this course we'll learn and implement about data collecyion and
+# processing of that data
+
