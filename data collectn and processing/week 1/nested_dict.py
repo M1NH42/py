3a7598032a67b4cbbc05f78a94e9013f203ec6b5
M1NH42 <minhazalam365@gmail.com>
2020-04-13 11:09:40 +0530

    Add code for extraction

--- data collectn and processing/week 1/nested_dict.py
+++ data collectn and processing/week 1/nested_dict.py
@@ -1,3 +1,23 @@
 # This program is the implemntation of the dictionary in a dictionary
 # known as nested dictionary or dictionary in a list of numbers etc..
 
+# define nested dictionary
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+        }
+
+color = info['personal_data']['physical_features']['color']['hair']
+
+print(color)
+
