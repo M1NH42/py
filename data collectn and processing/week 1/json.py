ed8a25251932297b902fc313a2f43e2455f2be78
M1NH42 <minhazalam365@gmail.com>
2020-04-13 11:28:19 +0530

    Add function pretty using dumps

--- data collectn and processing/week 1/json.py
+++ data collectn and processing/week 1/json.py
@@ -5,3 +5,13 @@
 import json
 
 # we'll basically see the working of loads() and dumps() methods
+
+#json.loads() takes a string as input and produces a python object
+# (a dictionary or a list) as output
+
+def pretty(obj) :
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2':{'b': 3, 'c': "yes"}}
+
+print(pretty(d))
