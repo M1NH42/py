1964133abc71df6d9dcdd0809a728bcf31a5fdd9
M1NH42 <minhazalam365@gmail.com>
2020-04-14 21:24:21 +0530

    Add logic

--- data_collectn_and_processing/nested_iteration.py
+++ data_collectn_and_processing/nested_iteration.py
@@ -2,3 +2,10 @@
 # * two for loops
 # * one loop and a square bracket(indexing)
 # * indexing(square bracket) and a for loop
+
+
+nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    for y in x:
+        print("     level2: {}".format(y))
