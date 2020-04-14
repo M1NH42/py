c55db15b07a945336b98e3b106d11f26240a4b77
M1NH42 <minhazalam365@gmail.com>
2020-04-14 21:26:43 +0530

    Add modified logic

--- data_collectn_and_processing/nested_iteration.py
+++ data_collectn_and_processing/nested_iteration.py
@@ -3,9 +3,17 @@
 # * one loop and a square bracket(indexing)
 # * indexing(square bracket) and a for loop
 
-
+# nested list
 nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+
+# iterate
 for x in nested1:
+
+    # outer loop
     print("level1: ")
-    for y in x:
-        print("     level2: {}".format(y))
+
+    if type(x) is list :
+        for y in x :
+            print("     level2: {}".format(y))
+    else :
+        print(x)
