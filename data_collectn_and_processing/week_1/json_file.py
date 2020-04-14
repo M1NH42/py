843c67d2680af4ed61de208d727997bd2236b8af
M1NH42 <minhazalam365@gmail.com>
2020-04-14 21:19:35 +0530

    Some changes

--- data_collectn_and_processing/week_1/json_file.py
+++ data_collectn_and_processing/week_1/json_file.py
@@ -2,29 +2,29 @@
 # # with python 3
 
 # # import module
-# import json
-
-# # we'll basically see the working of loads() and dumps() methods
-
-# #json.loads() takes a string as input and produces a python object
-# # (a dictionary or a list) as output
-
-# def pretty(obj) :
-#     return json.dumps(obj, sort_keys=True, indent=2)
-
-# d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2':{'b': 3, 'c': "yes"}}
-
-# print("****************** type of d ************************\n\n")
-# print(type(d))
-
-# print("\n\n****************** prints dictionary ************************\n\n")
-# print(d)
-
-# print("\n\n****************** after dumps function ************************\n\n")
-# print(pretty(d))
 import json
-entertainment = """[{"Library Data": {"count": 3500, "rows": 10, "locations": 3}}, {"Movie Theater Data": {"count": 8, "rows": 25, "locations": 2}}]"""
 
-d = json.loads(entertainment)
+# we'll basically see the working of loads() and dumps() methods
 
+#json.loads() takes a string as input and produces a python object
+# (a dictionary or a list) as output
+
+def pretty(obj) :
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2':{'b': 3, 'c': "yes"}}
+
+print("****************** type of d ************************\n\n")
+print(type(d))
+
+print("\n\n****************** prints dictionary ************************\n\n")
 print(d)
+
+print("\n\n****************** after dumps function ************************\n\n")
+print(pretty(d))
+# import json
+# entertainment = """[{"Library Data": {"count": 3500, "rows": 10, "locations": 3}}, {"Movie Theater Data": {"count": 8, "rows": 25, "locations": 2}}]"""
+
+# d = json.loads(entertainment)
+
+# print(d)
