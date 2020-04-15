515ce270b7585e0a4fa80da825dfed1f537c6ad2
M1NH42 <minhazalam365@gmail.com>
2020-04-15 14:13:15 +0530

    Comments for clarity

--- data_collectn_and_processing/week_2/py_map.py
+++ data_collectn_and_processing/week_2/py_map.py
@@ -3,14 +3,24 @@
 # of the list
 
 def double_stuff (things) :
+    """ Returns a new list with doubl ethe values than the previous list """
+
+    # accum list
     new_list = []
+
+    # iterate over each item in the prev list
     for thing in things :
+        # double each item
         new_elem = thing * 2
+        # add to the new list
         new_list.append(new_elem)
     return new_list
 
+# define list
 things = [2, 3, 5]
 
+# print
 print(things)
 
+# print by calling the defined function
 print(double_stuff(things))
