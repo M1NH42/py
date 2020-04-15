4212409b37e7a697aefa885e55f3cda1e941b75f
M1NH42 <minhazalam365@gmail.com>
2020-04-15 21:50:30 +0530

    Add program description

---
+++ data_collectn_and_processing/week_2/list_comp.py
@@ -0,0 +1,9 @@
+# Python provides an alternative way to do map and filter operations, called a list comprehension. 
+# Many programmers find them easier to understand and write. 
+# List comprehensions are concise ways to create lists from other lists. 
+# The general syntax is:
+# [<transformer_expression> for <loop_var> in <sequence> if <filtration_expression>]
+
+# nested dictionary
+
+tester = {'info': [{"name": "Lauren", 'class standing': 'Junior', 'major': "Information Science"},{'name': 'Ayo', 'class standing': "Bachelor's", 'major': 'Information Science'}, {'name': 'Kathryn', 'class standing': 'Senior', 'major': 'Sociology'}, {'name': 'Nick', 'class standing': 'Junior', 'major': 'Computer Science'}, {'name': 'Gladys', 'class standing': 'Sophomore', 'major': 'History'}, {'name': 'Adam', 'major': 'Violin Performance', 'class standing': 'Senior'}]}
