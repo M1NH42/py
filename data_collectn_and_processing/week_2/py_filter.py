a77e0feac96a5939b1a7bc8fd917631f1172ca17
M1NH42 <minhazalam365@gmail.com>
2020-04-15 16:56:56 +0530

    Comments for clarity

--- data_collectn_and_processing/week_2/py_filter.py
+++ data_collectn_and_processing/week_2/py_filter.py
@@ -4,6 +4,12 @@ new_num = list(map(lambda num : num *3, nums))
 
 print(new_num)
 
+
+# Python offers a more compact and general way to do it, the filter function. 
+# filter takes two arguments, a function and a sequence. The function takes one 
+# item and return True if the item should. It is automatically called for each item 
+# in the sequence. You don’t have to initialize an accumulator or iterate with a for loop.
+
 new_nums = list(filter(lambda num : num%2==0, nums))
 
 print(new_nums)
