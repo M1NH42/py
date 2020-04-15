b1388013e0c7d7990de8a81cec8814525c3ed769
M1NH42 <minhazalam365@gmail.com>
2020-04-15 14:28:49 +0530

    Some info!

--- data_collectn_and_processing/week_2/py_map.py
+++ data_collectn_and_processing/week_2/py_map.py
@@ -27,11 +27,22 @@ print(double_stuff(things))
 
 ####################### map() ##################################
 
+#  map takes two arguments, a function and a sequence. 
+# The function is the mapper that transforms items. 
+# It is automatically applied to each item in the sequence. 
+# You don’t have to initialize an accumulator or iterate with a for loop at all.
+
 states = ['hp','mp','up','tn','ap','asm','pb','mh']
 
 def f (st) :
     return st.upper()
 
+# Technically, in a proper Python 3 interpreter, the map function produces
+# an “iterator”, which is like a list but produces the items as they are needed. 
+# Most places in Python where you can use a list (e.g., in a for loop) you can use 
+# an “iterator” as if it was actually a list. So you probably won’t ever notice the difference. 
+# If you ever really need a list, you can explicitly turn the output of map into 
+# a list: list(map(...)).
 u_states = list(map(f, states))
 # u_states = map (lambda st: st.upper(), states)
 
