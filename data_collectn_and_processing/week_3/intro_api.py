bb2b3181b7ff7cf71afdd705d7a7548ef88f6d1d
M1NH42 <minhazalam365@gmail.com>
2020-04-19 13:40:07 +0530

    Add first program

---
+++ data_collectn_and_processing/week_3/intro_api.py
@@ -0,0 +1,31 @@
+# in this we'll be working with REST API
+
+# import modules
+import requests
+import json
+
+# get the page
+page = requests.get('https://api.datamuse.com/words?rel_rhy=funny')
+
+# type
+print(type(page))
+
+print(page.text[: 150]) # prints frist 150 charactersof th page
+
+print(page.url) # print the url that was fetched
+
+
+# turn page.text into pythn object
+page_obj = page.json()
+
+print()
+
+print(type(page_obj)) # must be python json object
+
+print()
+
+print(page_obj[0])
+
+print()
+
+print(json.dumps(page_obj, indent=2))
