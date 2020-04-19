629b4454bd93f54da76170bd8a71afd6c7dec98b
M1NH42 <minhazalam365@gmail.com>
2020-04-19 12:00:14 +0530

    Add list comprehension

--- data_collectn_and_processing/week_2/zip_listcomp.py
+++ data_collectn_and_processing/week_2/zip_listcomp.py
@@ -14,5 +14,30 @@ a = [1, 2, 4, 5]
 
 b = [5, 4, 3, 7]
 
-for i in range(len(a)):
-    print(a[i] + b[i])
+# Basic approach
+# for i in range(len(a)):
+#     print(a[i] + b[i])
+
+
+# above program can be done using zip()
+# make a list zips together
+# c = list(zip(a, b))
+
+# # print the list after zipping the two lists
+# print(c)
+
+# temp = []
+
+# # addition can be done by using zip
+# for (x, y) in c:
+#     temp.append(x+y)
+
+# print(temp)
+
+
+# addition and zipping can be combined together using list
+# comprehension and zip()
+# unpacking is also done using varibales x and y
+temp_list = [x+y for (x, y) in list(zip(a, b))]
+
+print(temp_list)
