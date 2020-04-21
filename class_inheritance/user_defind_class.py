953c5600048fa14ec6eec9a7eb92f21c668fca13
M1NH42 <minhazalam365@gmail.com>
2020-04-21 13:13:06 +0530

    Add user defined class

--- class_inheritance/user_defind_class.py
+++ class_inheritance/user_defind_class.py
@@ -1,3 +1,25 @@
 # In this module we will be working with the new type of paradigm ie
 # oop (object oriented programming) with classes and objets
 
+# creating class Point
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    # get value of x in the point
+    def get_x(self):
+        return self.x
+
+    # gets value y of the point
+    def get_y(self):
+        return self.y
+
+point1 = Point(12, 43)
+
+# setting instance of point()
+point2 = Point(32, 23)
+
+
+# calling get functions to get values of x and y
+print(list([point1.get_x(), point1.get_y()]))
