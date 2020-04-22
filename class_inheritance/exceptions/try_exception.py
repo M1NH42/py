43f76f92d2cbe3d5b4077db03ba542c642578d3e
M1NH42 <minhazalam365@gmail.com>
2020-04-22 14:53:03 +0530

    Add try and except

--- class_inheritance/exceptions/try_exception.py
+++ class_inheritance/exceptions/try_exception.py
@@ -7,3 +7,21 @@
 # except <ErrorType>:
 #    <exception handler code block>
 
+# def square(num):
+#     return num * num
+
+# # assertion testingg
+# assert square(3) == 9
+
+
+# try and except block of statement
+a_list = ['a', 'b']
+
+try :
+    # if something went wrong then in try block next statement will not be executed
+    third = a_list[2]
+except:
+    print("Third element does not exist")
+
+# after except statements are executed
+print("heyo")
