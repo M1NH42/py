8c2b51d0523256e5165ee7b271456df48b9e2ec5
M1NH42 <minhazalam365@gmail.com>
2020-04-22 13:36:56 +0530

    Add syntax for try block

--- class_inheritance/exceptions/try_exception.py
+++ class_inheritance/exceptions/try_exception.py
@@ -1,1 +1,9 @@
 # we'll see how this try and exception works in the python 3 programming language
+
+# syntax :
+
+# try:
+#    <try clause code block>
+# except <ErrorType>:
+#    <exception handler code block>
+
